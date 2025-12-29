836ec06ae33b87212bf60b7b62273aa365e65818
abumfxck <abumfxck@gmail.com>
2025-12-29 18:19:29 +0300

    2c

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -42,12 +42,19 @@ vim.pack.add({
   { src = "https://github.com/alexghergh/nvim-tmux-navigation" },
   { src = "https://github.com/webhooked/kanso.nvim" },
   { src = "https://github.com/metalelf0/black-metal-theme-neovim" },
+  { src = "https://github.com/hrsh7th/nvim-cmp" },
+  { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+  { src = "https://github.com/hrsh7th/cmp-buffer" },
+  { src = "https://github.com/hrsh7th/cmp-path" },
+  { src = "https://github.com/L3MON4D3/LuaSnip" },
+  { src = "https://github.com/saadparwaiz1/cmp_luasnip" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
 })
 
 -- Plugin initialization
 require "nvim-tmux-navigation".setup {}
 require "nvim-treesitter.configs".setup {
-  ensure_installed = { "c", "go", "vim", "vimdoc", "lua" },
+  ensure_installed = { "c", "go", "vim", "vimdoc", "lua", "json" },
   auto_install = true,
   highlight = {
     enable = true,
@@ -77,3 +84,63 @@ vim.api.nvim_create_autocmd('TermOpen', {
     vim.o.relativenumber = false
   end,
 })
+
+-- Autocomplete
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+    -- Навигация по меню
+    ["<C-n>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<C-p>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+    { name = "path" },
+  }),
+
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
